45540afc28c8f86c1eda59bfa1c12ba0bb246429
ZZC <zichun.zhou@sjsu.edu>
2020-09-10 20:57:22 -0700

    Finish with sentence

--- src/assignment1.java
+++ src/assignment1.java
@@ -1,5 +1,8 @@
 public class assignment1 {
     public static void main(String[] args) {
-        System.out.println("><)))°>");
+        System.out.println("                   ---------------");
+        System.out.println("                   |Hello         |");
+        System.out.println("><)))°>          --|World         |");
+        System.out.println("                   ---------------");
     }
 }
